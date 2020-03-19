911142e533a8ee5deb4c50f637f961d6f32c49d7
CharlesMacnab <60933720+CharlesMacnab@users.noreply.github.com>
2020-03-19 12:28:41 +0100

    Add files via upload

---
+++ TP3/cmake-build-debug/CMakeFiles/C__Vide.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Vide.dir/main.cpp.o"
+  "C__Vide.pdb"
+  "C__Vide"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Vide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
